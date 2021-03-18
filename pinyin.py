0b77bd983fd137f7c8a0d424028a2228c2b9d1ca
Colin <colin@ColinsMacBook-Pro.local>
2021-03-18 21:00:40 +0800

    add main program file pinyin.py

---
+++ pinyin.py
@@ -0,0 +1,31 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='Pinyin Input Method',
+    description='Pinyin to Chinese.',
+    allow_abbrev=True,
+)
+
+input_file_path = ''
+output_file_path = ''
+
+if __name__ == '__main__':
+
+    parser.add_argument('-i', '--input-file', dest=input_file_path, type=str, default='input.txt', help="Input file", required=True)
+    parser.add_argument('-o', '--output-file', dest=output_file_path, type=str, default='output.txt', help="Output file")
+    args = parser.parse_args()
+
+    # Load args
+    input_file_path = args.input_file_path
+    output_file_path = args.output_file_path
+
+    # process the input
+    with open(input_file_path) as input_file:
+        with open(output_file_path) as output_file:
+
+            while True:
+                line = input_file.readline()
+                if not line:
+                    break
+
+                output_file.write()
